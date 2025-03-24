2eea8390e7ba479aa5d78f46cb1f23f4e0632202
dian <nurdianto@duakelinci.co.id>
2025-03-24 10:54:57 +0700

    update ui dashboard

--- resources/views/pages/dashboard/index.blade.php
+++ resources/views/pages/dashboard/index.blade.php
@@ -17,52 +17,88 @@
 
     <section class="content">
         <div class="container-fluid">
-            <div class="row">
-                @foreach ($listDivisi as $divisi)
-                    <div class="col-12 col-lg-12">
-                        <div class="card">
-                            <div class="card-body m-0 p-0">
-                                @php
-                                    $totalMurid       = 0;
-                                    $totalMuridMale   = 0;
-                                    $totalMuridFemale = 0;
-                                @endphp
-                                <div class="small-box mb-0" style="background-color: white;">
-                                    <div class="inner bg-success text-center">
-                                        <h4 class="font-weight-bold">{{ $divisi->nama }}</h4>
-                                    </div>
-                                    <table class="table table-bordered table-sm table-striped mb-0">
-                                        <thead>
-                                            <tr class="text-center" style="font-size: 10x;">
-                                                <th>Kelas</th>
-                                                <th>Jumlah Siswa</th>
-                                                <th>Laki-laki</th>
-                                                <th>Perempuan</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach($divisi->listKelas as $kelas)
-                                                @php
-                                                    $totalMurid       += $kelas->listMurid->where('status', true)->count();
-                                                    $totalMuridMale   += $kelas->listMurid->where('jenis_kelamin', '1')->where('status', true)->count();
-                                                    $totalMuridFemale += $kelas->listMurid->where('jenis_kelamin', '0')->where('status', true)->count();
-                                                @endphp
+            <div class="accordion" id="divisi">
+                <div class="row">
+                    @foreach ($listDivisi as $index => $divisi)
+                        @php
+                            $width = (100 / count($listDivisi)) - 1 . "%";
+
+                            $totalMurid       = 0;
+                        @endphp
+                        @foreach($divisi->listKelas as $kelas)
+                            @php
+                                $totalMurid += $kelas->listMurid->where('status', true)->count();
+                            @endphp
+                        @endforeach
+                        <div style="margin: 0.5%; width: {{ $width }};">
+                            <!-- small box -->
+                            <div class="small-box bg-success">
+                                <div class="inner">
+                                    <h3>{{ $totalMurid }}<sup style="font-size: 20px"> Siswa</sup></h3>
+                                    <h5>{{ $divisi->nama }}</h5>
+                                </div>
+                                <div class="icon">
+                                    <i class="ion ion-stats-bars"></i>
+                                </div>
+                                <a class="small-box-footer" data-toggle="collapse" href="#{{ strtolower($divisi->nama) }}" role="button" aria-expanded="{{ $index === 0 ? "true" : "false" }}" aria-controls="{{ strtolower($divisi->nama) }}">
+                                    Detail <i class="fas fa-arrow-circle-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                @foreach ($listDivisi as $index => $divisi)
+                    <div class="collapse {{ $index === 0 ? "show" : "" }}" id="{{ strtolower($divisi->nama) }}" aria-labelledby="{{ strtolower($divisi->nama) }}" data-parent="#divisi">
+                        <div class="card card-body m-0 p-0 mb-3">
+                            @php
+                                $number = 0;
+
+                                $totalMurid       = 0;
+                                $totalMuridMale   = 0;
+                                $totalMuridFemale = 0;
+                            @endphp
+                            <div class="small-box m-0 p-0">
+                                <div class="inner bg-success">
+                                    <h6 class="font-weight-bold"><i class="fa fa-users" aria-hidden="true"></i> {{ $divisi->nama }}</h6>
+                                </div>
+                                <table class="table table-hover text-nowrap m-0 p-0">
+                                    <thead class="text-center">
+                                        <tr>
+                                            <th rowspan="2" class="align-middle">No</th>
+                                            <th rowspan="2" class="align-middle">Kelas</th>
+                                            <th colspan="3">Sensus Siswa</th>
+                                        </tr>
+                                        <tr>
+                                            <th>Laki - Laki</th>
+                                            <th>Perempuan</th>
+                                            <th>Total</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($divisi->listKelas as $index => $kelas)
+                                            @php
+                                                $totalMurid       += $kelas->listMurid->where('status', true)->count();
+                                                $totalMuridMale   += $kelas->listMurid->where('jenis_kelamin', '1')->where('status', true)->count();
+                                                $totalMuridFemale += $kelas->listMurid->where('jenis_kelamin', '0')->where('status', true)->count();
+                                            @endphp
+                                            @if ($kelas->listMurid->where('status', true)->count() > 0)
                                                 <tr>
-                                                    <td style="width: 40%;">{{ $kelas->nama }}</td>
-                                                    <td style="width: 20%;" class="text-center">{{ $kelas->listMurid->where('status', true)->count() }}</td>
+                                                    <td style="width: 5%;">{{ ++$number }}</td>
+                                                    <td style="width: 35%;">{{ $kelas->nama }}</td>
                                                     <td style="width: 20%;" class="text-center">{{ $kelas->listMurid->where('jenis_kelamin', '1')->where('status', true)->count() }}</td>
                                                     <td style="width: 20%;" class="text-center">{{ $kelas->listMurid->where('jenis_kelamin', '0')->where('status', true)->count() }}</td>
+                                                    <td style="width: 20%;" class="text-center">{{ $kelas->listMurid->where('status', true)->count() }}</td>
                                                 </tr>
-                                            @endforeach
-                                        </tbody>
-                                        <tfoot>
-                                            <th class="text-center">Total</th>
-                                            <th class="text-center">{{ $totalMurid }}</th>
-                                            <th class="text-center">{{ $totalMuridMale }}</th>
-                                            <th class="text-center">{{ $totalMuridFemale }}</th>
-                                        </tfoot>
-                                    </table>
-                                </div>
+                                            @endif
+                                        @endforeach
+                                    </tbody>
+                                    <tfoot>
+                                        <th class="text-center" colspan="2">Total</th>
+                                        <th class="text-center">{{ $totalMuridMale }}</th>
+                                        <th class="text-center">{{ $totalMuridFemale }}</th>
+                                        <th class="text-center">{{ $totalMurid }}</th>
+                                    </tfoot>
+                                </table>
                             </div>
                         </div>
                     </div>
